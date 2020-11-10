bce386d1a58657421952290ac65375009e3a8d9b
A-Jordan-95 <arj14b@my.fsu.edu>
2020-11-10 14:08:45 -0500

    updated web app to display data from mongodb

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var monk = require('monk');
+var db = monk('localhost:27017/analytics_web_page');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,41 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Analytics tracker for alexjordan.page' });
+});
+
+
+/* GET region analytics page. */
+router.get('/regionlist', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('regioncollection');
+  collection.find({},{},function(e,docs) {
+    res.render('regionlist', {
+      "regionlist" : docs
+    });
+  });
+});
+
+/* GET opsys analytics page. */
+router.get('/opsyslist', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('opsyscollection');
+  collection.find({},{},function(e,docs) {
+    res.render('opsyslist', {
+      "opsyslist" : docs
+    });
+  });
+});
+
+/* GET visitors analytics page. */
+router.get('/uniquevisitors', function(req, res) {
+  var db = req.db;
+  var collection = db.get('visitorscollection');
+  collection.find({},{},function(e,docs) {
+    res.render('uniquevisitors', {
+      "uniquevisitors" : docs
+    });
+  });
 });
 
 module.exports = router;
